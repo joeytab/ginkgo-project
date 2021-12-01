dda159420dc354506e71f3b75b056c69efe348da
joeyta <jbanerje@caltech.edu>
2021-11-30 20:35:15 -0800

    initial frontend working

--- django/frontend/src/App.js
+++ django/frontend/src/App.js
@@ -9,7 +9,8 @@ class App extends Component{
     super(props);
     this.state = {
       prevSearches: [],
-      currSeq: {sequence: "", alignments: []}
+      currSeq: {sequence: "", alignments: []},
+      tableHtml: ""
     };
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -40,37 +41,41 @@ class App extends Component{
       currSeq
     });
   }
-
-renderItems = (results) => {
+renderItems = (results) =>{
+  // console.log(results);
+  // console.log("print first thing")
+  // console.log(results[0]);
   return (
-    <Table singleLine>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell>Sequence</Table.HeaderCell>
-          <Table.HeaderCell>E value</Table.HeaderCell>
-          <Table.HeaderCell>Length</Table.HeaderCell> 
-          <Table.HeaderCell>Query</Table.HeaderCell>
-          <Table.HeaderCell>Match</Table.HeaderCell>
-          <Table.HeaderCell>Subject</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
+    
+  <Table singleLine>
+    <Table.Header>
+      <tr>
+        <th>Sequence</th>
+        <th>E value</th>
+        <th>Length</th> 
+        <th>Query</th>
+        <th>Match</th>
+        <th>Subject</th>
+      </tr>
+    </Table.Header>
 
-      <Table.Body>
-        {results.map(el => {
-          console.log(el);
-           return(
-            <Table.Row key={el.sequence}>
-              <Table.Cell> {el.sequence} </Table.Cell>
-              <Table.Cell>{el.e_val}</Table.Cell>
-              <Table.Cell> {el.length} </Table.Cell>
-              <Table.Cell> {el.query} </Table.Cell>
-              <Table.Cell> {el.match} </Table.Cell>
-              <Table.Cell> {el.subject} </Table.Cell>
-            </Table.Row>
-          );
-        })}
-      </Table.Body>
-    </Table>
+    <Table.Body>
+      {/* {results.map(el => { */}
+        {/* console.log("item");
+        console.log(el);
+          return( */} 
+          <Table.Row >
+            <Table.Cell> {results[0].sequence} </Table.Cell>
+            <Table.Cell>{results[0].e_val}</Table.Cell>
+            <Table.Cell> {results[0].length} </Table.Cell>
+            <Table.Cell> {results[0].query} </Table.Cell>
+            <Table.Cell> {results[0].match} </Table.Cell>
+            <Table.Cell> {results[0].subject} </Table.Cell>
+          </Table.Row>
+      {/* )}; */}
+      {/* }) } */}
+    </Table.Body>
+  </Table>
   );
 };
 
@@ -85,20 +90,31 @@ renderItems = (results) => {
     this.setState({"alignments" : data});
   
     this.state.prevSearches.push(data);
-    console.log("prev searches");
-    console.log(this.state.prevSearches);
+    console.log(this.state.prevSearches)
+    const table = this.renderAlignments();
+    this.setState({"tableHtml": table});
+    // console.log("prev searches");
+    // console.log(this.state.prevSearches);
     // return(this.renderAlignments());
   };
 
-  renderAlignments = () => {
+  renderAlignments(){
     console.log("render alignments");
     const results = this.state.prevSearches;
+    // const results = [[{"e_val": 1.06557e-26,
+    //   "length": 2184,
+    //   "match": "|||||| ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||",
+    //   "query": "GTTGGTAATAAATCTCACAAAACTCTAAGGACAAACTCTGGCAGAAATCCTAAGGAGAAACATTA",
+    //   "sequence": "gi|323371|gb|M55319.1|CHVTERMRE Paramecium bursaria Chlorella virus 1 terminal inverted repeat sequence",
+    //   "subject": "GTTGGTGATAAATCTCACAAAACTCTAAGGACAAACTCTGGCAGAAATCCTAAGGAGAAACATTA"}]];
+    console.log(results);
+    console.log(results[0]);
     if(results.length == 0){
       console.log("No searches yet")
     }
     return(
       results.map(el => {
-        return this.renderItems(el);
+        return (this.renderItems(el));
       })
     );
   }
@@ -123,9 +139,9 @@ render() {
           </label>
         <button onClick={this.handleSubmit}
         > Query </button>
-       <div>
-         {this.renderAlignments()}
-       </div>
+        {this.state.tableHtml}
+        {/* <div dangerouslySetInnerHTML={{__html: this.state.tableHtml}} /> */}
+        
     </main>
   );
 };
