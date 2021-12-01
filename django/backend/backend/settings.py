b4ddb0574efbaf47be477afa88ae705c72611397
joeyta <jbanerje@caltech.edu>
2021-12-01 00:29:13 -0800

    revert commit

--- django/backend/backend/settings.py
+++ django/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9p#4+u)vd!@jzrp1$_p^u=i_ofv+*fmx9k@*o*k%rk75kfxip-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '10.8.2.105']
 
 
 # Application definition
